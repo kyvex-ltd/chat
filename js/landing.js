df151235627f40cd29122b5404dbc21df6d3d12d
BeauTheBeau <beauthebeau@protonmail.me>
2023-06-02 20:07:08 +0100

    Moved theme switching logic
    Theme switching is now in theme-switcher.js and listens for changes in local storage, so if the theme is changed in one place it is changed everywhere else.
    
    Signed-off-by: BeauTheBeau <beauthebeau@protonmail.me>

--- js/landing.js
+++ js/landing.js
@@ -8,49 +8,7 @@ window.addEventListener("scroll", () => {
   else NAVBAR.style.top = "-256px";
 });
 
-// THEME MANAGER
-const
-  THEME_ICON = document.getElementById("theme-toggle"),
-  NAV = document.getElementById("nav");
-
-let
-  THEME = localStorage.getItem("theme");
-
-localStorage.setItem("theme", THEME);
-
-if (THEME === "dark") {
-  document.body.classList.add("dark");
-  THEME_ICON.classList.replace("ai-sun-fill", "ai-moon-fill");
-} else {
-  document.body.classList.remove("dark");
-  document.body.classList.add("light");
-  THEME_ICON.classList.replace("ai-moon-fill", "ai-sun-fill");
-}
-
-function change_theme() {
-  if (THEME === "dark") {
-    document.body.classList.remove("dark");
-    document.body.classList.add("light");
-    THEME_ICON.classList.replace("ai-moon-fill", "ai-sun-fill");
-    THEME = "light";
-  } else {
-    document.body.classList.remove("light");
-    document.body.classList.add("dark");
-    THEME_ICON.classList.replace("ai-sun-fill", "ai-moon-fill");
-    THEME = "dark";
-  }
-  localStorage.setItem("theme", THEME);
-}
-
-// When theme icon is clicked or enter key is pressed
-THEME_ICON.addEventListener("click", change_theme);
-THEME_ICON.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") change_theme();
-});
-
 // If an element in the nav is focussed, make sure the nav is visible
-NAV.addEventListener("focusin", () => {
+NAVBAR.addEventListener("focusin", () => {
   NAVBAR.style.top = "0";
 });
-
-// NAV BAR
--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -1,2 +1,25 @@
+const THEME_ICON = document.getElementById("theme-toggle"), BODY = document.body;
+let THEME = localStorage.getItem("theme");
 
+function toggleTheme() {
+  if (THEME === "dark") {
+    THEME = "light";
+    BODY.classList.remove("dark");
+    BODY.classList.add("light");
+    THEME_ICON.classList.replace("ai-moon-fill", "ai-sun-fill");
 
+  } else {
+    THEME = "dark";
+    BODY.classList.remove("light");
+    BODY.classList.add("dark");
+    THEME_ICON.classList.replace("ai-sun-fill", "ai-moon-fill");
+  }
+  localStorage.setItem("theme", THEME);
+}
+
+THEME_ICON.addEventListener("click", toggleTheme);
+
+// listen for changes in local storage
+window.addEventListener("storage", (event) => {
+  if (event.key === "theme") toggleTheme();
+});
