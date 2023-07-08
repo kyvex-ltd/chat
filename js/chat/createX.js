de8b30a95fe4cf202391cd929b59db6801a35b03
BeauTheBeau <beauthebeau@protonmail.me>
2023-07-09 00:19:18 +0100

    Improve CSS readability and expand chat functionality
    Removed unnecessary white-spaces in index.css to improve readability and left a cleaner look. Functions to createCommunity, createCategory, and createChannel in chat/createX.js have been expanded with detailed comments and proper implementation replacing placeholders. Introduced a smooth transition effect on chat window resizing to enhance the user experience. A new markdown file has been added, providing a user-friendly, readable description of application features. Updates have been propagated accordingly in the accompanying .scss and .css.map files.
    
    Signed-off-by: BeauTheBeau <beauthebeau@protonmail.me>

--- js/chat/createX.js
+++ js/chat/createX.js
@@ -4,6 +4,17 @@ import {getQueryParam, url,} from './misc.js';
 const communityContainer = document.querySelector(`#community__view`);
 const createCommunityContainer = document.querySelector(`#dialog__create__community`);
 
+/**
+ * Create a community
+ * @returns {Promise<void>}
+ * @constructor
+ * @param {string} name
+ * @param {string} description
+ * @description Makes a POST request to the API to create a community with the given name and description and Bearer token
+ * @example createCommunity("Kyvex", "The official Kyvex community")
+ * @see {@link https://kyvex.co.uk/chat_api/docs/Community/createCommunity.html} (not yet created) for more information
+ */
+
 export function createCommunity() {
   const name = document.querySelector(`#create__community__name`).value;
   const description = document.querySelector(`#create__community__description`).value;
@@ -38,8 +49,19 @@ export function createCommunity() {
   }
 }
 
+/**
+ * Create a category
+ * @returns {Promise<void>}
+ * @constructor
+ * @param {string} name
+ * @param {string} description
+ * @param {string} communityId - The ID of the community to create the category in
+ * @description Makes a POST request to the API to create a category with the given name,
+ * description and community ID and Bearer token
+ * @example createCategory("General", "General chat", "1234567890")
+ * @see {@link https://kyvex.co.uk/chat_api/docs/Category/createCategory.html} (not yet created) for more information
+ */
 export function createCategory() {
-  return alert(`501: This feature is not yet implemented`);
   const name = document.querySelector(`#create__category__name`).value;
   const description = document.querySelector(`#create__category__description`).value;
   const communityId = getQueryParam("community");
@@ -47,7 +69,6 @@ export function createCategory() {
   if (name === `` || communityId === ``) alert(`Please enter a name and select a community`);
 
   else {
-    // Fetch
     fetch(`${url}/api/v1/category/create`, {
       method: `POST`,
       headers: {
@@ -60,20 +81,59 @@ export function createCategory() {
       .then(res => {
         console.log(res)
         if (res.category.name) {
-          addCatego
-          ry(res.category, communityContainer);
+          addCategory(res.category, communityContainer);
           createCategoryContainer.close();
         } else {
           createCategoryContainer.close();
           alert("Couldn't create category, please try again later");
         }
-
       })
       .catch(err => console.error(err));
   }
 }
 
+
+/**
+ * Create a channel
+ * @returns {Promise<void>}
+ * @constructor
+ * @param {string} name
+ * @param {string} description
+ * @param {string} categoryId - The ID of the category to create the channel in
+ * @description Makes a POST request to the API to create a channel with the given name,
+ * description and category ID and Bearer token.
+ * If there is no category, it will be added into the "Misc." category
+ * @example createChannel("General", "General chat", "1234567890") // Creates a channel called "General" in the category with the ID "1234567890"
+ * @see {@link https://kyvex.co.uk/chat_api/docs/Channel/createChannel.html} (not yet created) for more information
+ */
 export function createChannel() {
-  return alert(`501: This feature is not yet implemented`);
   const name = document.querySelector(`#create__channel__name`).value;
+  const description = document.querySelector(`#create__channel__description`).value || ``;
+  const categoryId = getQueryParam("category");
+
+  if (name === `` || categoryId === ``) return alert(`Please enter a name and select a category`);
+
+  else {
+    fetch(`${url}/api/v1/channel/create`, {
+      method: `POST`,
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem(`token`)}`,
+        'Content-Type': `application/json`
+      },
+      body: JSON.stringify({name, description, categoryId})
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        if (res.channel.name) {
+          addChannel(res.channel, communityContainer);
+          createChannelContainer.close();
+        } else {
+          createChannelContainer.close();
+          alert("Couldn't create channel, please try again later");
+        }
+      })
+      .catch(err => console.error(err));
+  }
 }
+
